2fe94e6ebe09a70999f6d812a7f52484188cc237
skvoruha <skvoruha@mail.ru>
2022-02-04 00:19:54 +0300

    create tag header

---
+++ js/script.js
@@ -0,0 +1,141 @@
+'use strict';
+
+const a = 10;
+
+console.log(a);
+
+// let answer = prompt("Тебе есть 18 лет ?", "18 лет");
+// console.log(answer);
+
+// console.log(5 + '5');
+
+// console.log(5 - '5');
+// console.log(5 * '5');
+// console.log(5 / '5ty');
+
+// console.log(5 == '5');
+// console.log(5 === '5');
+
+// console.log(typeof String(true));
+// console.log(typeof String(undefined));
+// console.log(typeof String(null));
+// console.log(typeof String("123"));
+// console.log(typeof String(123));
+
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number(undefined));
+// console.log(Number(null));
+// console.log(Number("123"));
+// console.log(Number("123dfs"));
+// console.log(Number(123));
+
+
+// console.log(Boolean(""));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+// console.log(Boolean(false));
+
+// let a = 10;
+
+// console.log(Boolean(!!5));
+// console.log(10 + '');
+// console.log(a.toString());
+
+// console.log(+"10c");
+// console.log(+"10");
+
+// let str = +prompt("Cколько тебе лет?");
+// let result = str + 10;
+
+// console.log(result);
+
+
+// let str = prompt("Cколько тебе лет?");
+
+// console.log(parseInt(str));
+
+// console.log(parseInt('11111', 2));
+
+// if (true) console.log(' Истина');
+// if (false) console.log(' Ложь');
+
+// let n =14;
+
+// if (n == 11) {
+//   console.log(' Команда 1');
+//   console.log(' Команда 2');
+//   console.log(' Команда 3');
+// } else if (n == 12) {
+//   console.log(' Команда 3');
+//   console.log(' Команда 4');
+//   console.log(' Команда 5');
+// } else if (n == 13) {
+//   console.log(' Команда 6');
+//   console.log(' Команда 7');
+//   console.log(' Команда 8');
+// } else  {
+//   console.log(' Команда 9');
+//   console.log(' Команда 10');
+//   console.log(' Команда 11');
+// }
+// let n = 12;
+// switch (n) {
+//   case 3:
+//     console.log(3);
+//     break;
+//   case 4:
+//     console.log(4);
+//     break;
+//   case 10:
+//     console.log(10);
+//     break;
+//   case 11:
+//   case 12:
+//       console.log(11);
+//     break;
+//   default:
+//     console.log(' Не верно ни одно значение');
+// }
+// const n = +prompt(' Введите число : ');
+
+// switch (n) {
+  //   case 3:
+  //     console.log(3);
+  //     break;
+  //   case 4:
+  //     console.log(4);
+  //     break;
+  //   case 10:
+  //     console.log(10);
+  //     break;
+  //   case 11:
+  //   case 12:
+  //       console.log(11);
+  //     break;
+  //   default:
+  //     console.log(' Не верно ни одно значение');
+  // }
+
+//   const n = +prompt(' Введите число : ');
+
+
+// switch (true) {
+//   case 5 < n && n < 10:
+//     console.log('n > 10');
+//     break;
+//   default:
+//       console.log(' Не верно ни одно значение');
+// }
+
+// const n = +prompt(' Введите число : ');
+
+// let result = n === 10 ? 1 : 2
+// console.log(result);
+
+
+// const n = +prompt(' Введите число : ');
+
+// n === 10 ? console.log(" верно") : console.log(" не верно");;
+
