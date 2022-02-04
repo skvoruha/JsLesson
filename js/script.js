8dcdc9707cf831660840a79c88763f6e5af23b35
skvoruha <skvoruha@mail.ru>
2022-02-05 01:20:29 +0300

    create lesson05

--- js/script.js
+++ js/script.js
@@ -1,141 +1,50 @@
-'use strict';
+'use strict'
+// // рекурския
+// const potato = 10
 
-const a = 10;
-
-console.log(a);
-
-// let answer = prompt("Тебе есть 18 лет ?", "18 лет");
-// console.log(answer);
-
-// console.log(5 + '5');
-
-// console.log(5 - '5');
-// console.log(5 * '5');
-// console.log(5 / '5ty');
-
-// console.log(5 == '5');
-// console.log(5 === '5');
-
-// console.log(typeof String(true));
-// console.log(typeof String(undefined));
-// console.log(typeof String(null));
-// console.log(typeof String("123"));
-// console.log(typeof String(123));
-
-// console.log(Number(true));
-// console.log(Number(false));
-// console.log(Number(undefined));
-// console.log(Number(null));
-// console.log(Number("123"));
-// console.log(Number("123dfs"));
-// console.log(Number(123));
-
-
-// console.log(Boolean(""));
-// console.log(Boolean(undefined));
-// console.log(Boolean(null));
-// console.log(Boolean(NaN));
-// console.log(Boolean(false));
-
-// let a = 10;
-
-// console.log(Boolean(!!5));
-// console.log(10 + '');
-// console.log(a.toString());
-
-// console.log(+"10c");
-// console.log(+"10");
-
-// let str = +prompt("Cколько тебе лет?");
-// let result = str + 10;
-
-// console.log(result);
-
-
-// let str = prompt("Cколько тебе лет?");
-
-// console.log(parseInt(str));
-
-// console.log(parseInt('11111', 2));
-
-// if (true) console.log(' Истина');
-// if (false) console.log(' Ложь');
-
-// let n =14;
-
-// if (n == 11) {
-//   console.log(' Команда 1');
-//   console.log(' Команда 2');
-//   console.log(' Команда 3');
-// } else if (n == 12) {
-//   console.log(' Команда 3');
-//   console.log(' Команда 4');
-//   console.log(' Команда 5');
-// } else if (n == 13) {
-//   console.log(' Команда 6');
-//   console.log(' Команда 7');
-//   console.log(' Команда 8');
-// } else  {
-//   console.log(' Команда 9');
-//   console.log(' Команда 10');
-//   console.log(' Команда 11');
-// }
-// let n = 12;
-// switch (n) {
-//   case 3:
-//     console.log(3);
-//     break;
-//   case 4:
-//     console.log(4);
-//     break;
-//   case 10:
-//     console.log(10);
-//     break;
-//   case 11:
-//   case 12:
-//       console.log(11);
-//     break;
-//   default:
-//     console.log(' Не верно ни одно значение');
-// }
-// const n = +prompt(' Введите число : ');
-
-// switch (n) {
-  //   case 3:
-  //     console.log(3);
-  //     break;
-  //   case 4:
-  //     console.log(4);
-  //     break;
-  //   case 10:
-  //     console.log(10);
-  //     break;
-  //   case 11:
-  //   case 12:
-  //       console.log(11);
-  //     break;
-  //   default:
-  //     console.log(' Не верно ни одно значение');
-  // }
-
-//   const n = +prompt(' Введите число : ');
-
-
-// switch (true) {
-//   case 5 < n && n < 10:
-//     console.log('n > 10');
-//     break;
-//   default:
-//       console.log(' Не верно ни одно значение');
+// const peelPotato = function (potatoIndex) {
+//   if (potatoIndex > 0){
+//     console.log('Картошка №' + potatoIndex + ' почищена');
+//     potatoIndex--
+//     peelPotato(potatoIndex)
+//   }
 // }
 
-// const n = +prompt(' Введите число : ');
+// peelPotato(potato)
+let potato = 10
 
-// let result = n === 10 ? 1 : 2
-// console.log(result);
+// for (let i = 10; i > 0; i--){
+//     console.log('Картошка №' + i + ' почищена');
+// }
+
+// for (let i = 1; i < 11; i++){
+//     console.log('Картошка №' + i + ' почищена');
+// }
+
+// while (potato > 0) {
+//   console.log('Картошка №' + potato + ' почищена');
+//   potato--
+// }
+
+// do {
+//   console.log('Картошка №' + potato + ' почищена');
+//   potato--
+// }
+// while (false)
 
 
-// const n = +prompt(' Введите число : ');
+// for (let i = 10; i > 0; i--){
+//   if(i === 5) continue
+//     console.log('Картошка №' + i + ' почищена');
+// }
 
-// n === 10 ? console.log(" верно") : console.log(" не верно");;
 
+// for (let i = 10; i > 0; i--){
+//   if(i === 5) break
+//     console.log('Картошка №' + i + ' почищена');
+// }
+
+
+for (let i = 1; i < 11; i++){
+  if(i !== 5)  console.log('Картошка №' + i + ' почищена');
+}
