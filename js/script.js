04f2d8354314721d7d360624050e2232c3d7a09e
skvoruha <skvoruha@mail.ru>
2022-02-04 18:00:18 +0300

    create lesson 04

--- js/script.js
+++ js/script.js
@@ -1,141 +1,213 @@
 'use strict';
 
-const a = 10;
-
-console.log(a);
-
-// let answer = prompt("Тебе есть 18 лет ?", "18 лет");
-// console.log(answer);
-
-// console.log(5 + '5');
-
-// console.log(5 - '5');
-// console.log(5 * '5');
-// console.log(5 / '5ty');
-
-// console.log(5 == '5');
-// console.log(5 === '5');
-
-// console.log(typeof String(true));
-// console.log(typeof String(undefined));
-// console.log(typeof String(null));
-// console.log(typeof String("123"));
-// console.log(typeof String(123));
-
-// console.log(Number(true));
-// console.log(Number(false));
-// console.log(Number(undefined));
-// console.log(Number(null));
-// console.log(Number("123"));
-// console.log(Number("123dfs"));
-// console.log(Number(123));
-
-
-// console.log(Boolean(""));
-// console.log(Boolean(undefined));
-// console.log(Boolean(null));
-// console.log(Boolean(NaN));
-// console.log(Boolean(false));
-
-// let a = 10;
-
-// console.log(Boolean(!!5));
-// console.log(10 + '');
-// console.log(a.toString());
-
-// console.log(+"10c");
-// console.log(+"10");
-
-// let str = +prompt("Cколько тебе лет?");
-// let result = str + 10;
-
-// console.log(result);
-
-
-// let str = prompt("Cколько тебе лет?");
-
-// console.log(parseInt(str));
-
-// console.log(parseInt('11111', 2));
-
-// if (true) console.log(' Истина');
-// if (false) console.log(' Ложь');
-
-// let n =14;
-
-// if (n == 11) {
-//   console.log(' Команда 1');
-//   console.log(' Команда 2');
-//   console.log(' Команда 3');
-// } else if (n == 12) {
-//   console.log(' Команда 3');
-//   console.log(' Команда 4');
-//   console.log(' Команда 5');
-// } else if (n == 13) {
-//   console.log(' Команда 6');
-//   console.log(' Команда 7');
-//   console.log(' Команда 8');
-// } else  {
-//   console.log(' Команда 9');
-//   console.log(' Команда 10');
-//   console.log(' Команда 11');
+// function getMessage(){
+//   console.log("Hellow world");
 // }
-// let n = 12;
-// switch (n) {
-//   case 3:
-//     console.log(3);
-//     break;
-//   case 4:
-//     console.log(4);
-//     break;
-//   case 10:
-//     console.log(10);
-//     break;
-//   case 11:
-//   case 12:
-//       console.log(11);
-//     break;
-//   default:
-//     console.log(' Не верно ни одно значение');
-// }
-// const n = +prompt(' Введите число : ');
+// getMessage();
+// getMessage();
+// getMessage();
 
-// switch (n) {
-  //   case 3:
-  //     console.log(3);
-  //     break;
-  //   case 4:
-  //     console.log(4);
-  //     break;
-  //   case 10:
-  //     console.log(10);
-  //     break;
-  //   case 11:
-  //   case 12:
-  //       console.log(11);
-  //     break;
-  //   default:
-  //     console.log(' Не верно ни одно значение');
-  // }
+// function clearHandsOperation(){
+//   function dirtyHands(){
+//     console.log('Испачкать руки');
+//   }
 
-//   const n = +prompt(' Введите число : ');
+//   function goToTheBathroom(){
+//      console.log('Пойти в ваннюу комнату');
+//   }
 
+//   function cleanHands(){
+//     console.log("Помыть руки");
+//   }
+//   dirtyHands();
+//   goToTheBathroom();
+//   cleanHands();
 
-// switch (true) {
-//   case 5 < n && n < 10:
-//     console.log('n > 10');
-//     break;
-//   default:
-//       console.log(' Не верно ни одно значение');
+//   dirtyHands();
+//   goToTheBathroom();
+//   cleanHands();
 // }
 
-// const n = +prompt(' Введите число : ');
-
-// let result = n === 10 ? 1 : 2
-// console.log(result);
+// clearHandsOperation();
 
 
-// const n = +prompt(' Введите число : ');
 
-// n === 10 ? console.log(" верно") : console.log(" не верно");;
+// function clearHandsOperation(){
+//   function step(index, str){
+//      console.log(index + " " +str);
+//   }
 
+//   step(1, 'Испачкать руки');
+//   step(2, 'Пойти в ваннюу комнату');
+//   step(3, 'Помыть руки');
+// }
+
+// clearHandsOperation();
+
+
+
+// const cleanHandsOperation = function(){
+//   const step = function(index, str){
+//      console.log(index + " " +str);
+//   }
+
+//   step(1, 'Испачкать руки');
+//   step(2, 'Пойти в ваннюу комнату');
+//   step(3, 'Помыть руки');
+// }
+
+// cleanHandsOperation();
+
+
+// const cleanHandsOperation = function(){
+//   // const step = function(index, str){
+//   //    console.log(index + " " +str);
+//   // }
+//   const step = new Function('index' , 'str', 'console.log(str)')
+//   step(1, 'Испачкать руки');
+//   step(2, 'Пойти в ваннюу комнату');
+//   step(3, 'Помыть руки');
+// }
+
+// cleanHandsOperation();
+
+
+// const countSum = function(a,b) {
+//   console.log(a+b);
+// }
+
+// countSum(3,4)
+
+// let res;
+
+// const countSum = function(a, b) {
+//   return a + b;
+// }
+
+
+// res = countSum(3, 4)
+// console.log(res);
+
+// const countSum = function(a, b) {
+//   return a + b;
+// }
+
+// console.log(countSum(3,4) + 10);
+
+
+// const countSum = function(a, b = 0) {
+//   console.log(arguments[6]);
+//   return a + b;
+// }
+
+// console.log(countSum(33 ,234,34,34,1,4,34,35));
+
+
+// const countSum = function(a, b, callback) {
+//   return a + callback(b);
+// }
+// const secondFunc = function(num){
+//   return num + 2;
+// }
+
+// console.log(countSum(2 ,4 , secondFunc));
+
+// function one() {
+//   console.log('one');
+// }
+
+// const two = function(){
+//   console.log('two');
+// }
+
+// console.log(one);
+// console.log(two);
+
+
+// const countSum = function(a, b, callback) {
+//   return a + callback(b);
+// }
+
+// console.log(countSum(2 ,4 , function(num){
+//   return num + 2;
+// }));
+
+
+// const money =50000;
+// const expenses1 = 200;
+// const expenses2 = 800;
+
+// const sumExpenses = function(exp1,exp2){
+//   return exp1+exp2;
+// }
+
+// const countTotal = function(){
+//   return money-sumExpenses(expenses1,expenses2)
+// }
+
+// console.log(countTotal());
+
+// const money =50000;
+// const expenses1 = 200;
+// const expenses2 = 800;
+
+// const countTotal = function(count, call){
+//   return count - call(expenses1,expenses2)
+// }
+
+// console.log(countTotal(money, function(exp1,exp2){
+//   return exp1+exp2;
+// }
+// ));
+
+// const sendServer = function(call){
+//   setTimeout(function(){
+//     console.log('Вызов прошёл');
+//     call()
+//   }, 2000)
+// }
+
+// const someFunc = function(){
+//   console.log('А теперь отработала вторая функция');
+// }
+
+// sendServer(someFunc)
+
+
+//  СТИЛИ КОДА ПРАВИЛЬНОЕ НАПИСАНИЕ КОДА
+// блок объявления переменных
+const button = document.querySelector('#btn')
+
+const a = 1
+const b = 'second'
+
+let count = 0
+let array = []
+
+let g,h,j
+
+// блок описания функций
+function foo(){
+  const с = 'c'
+}
+
+function boo(){
+  const d = 'd'
+}
+
+//  блок функционала , функциональный блок
+button.addEventListener('click', function(){
+  console.log("Clicked");
+})
+
+g = 3567
+
+if (a === 1) console.log(a);
+
+foo()
+boo()
+
+// блок вывода в консоль , мусорный блок
+console.log(b);
+console.log(array);
+alert(count)
