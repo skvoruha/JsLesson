68aad53a13c4b66a395f52ec7ff2ce87dad6144f
skvoruha <skvoruha@mail.ru>
2022-02-07 17:18:03 +0300

    made lesson06

--- js/script.js
+++ js/script.js
@@ -1,141 +1,114 @@
-'use strict';
+'use strict'
 
-const a = 10;
+// let a = 5
 
-console.log(a);
-
-// let answer = prompt("Тебе есть 18 лет ?", "18 лет");
-// console.log(answer);
-
-// console.log(5 + '5');
-
-// console.log(5 - '5');
-// console.log(5 * '5');
-// console.log(5 / '5ty');
-
-// console.log(5 == '5');
-// console.log(5 === '5');
-
-// console.log(typeof String(true));
-// console.log(typeof String(undefined));
-// console.log(typeof String(null));
-// console.log(typeof String("123"));
-// console.log(typeof String(123));
-
-// console.log(Number(true));
-// console.log(Number(false));
-// console.log(Number(undefined));
-// console.log(Number(null));
-// console.log(Number("123"));
-// console.log(Number("123dfs"));
-// console.log(Number(123));
-
-
-// console.log(Boolean(""));
-// console.log(Boolean(undefined));
-// console.log(Boolean(null));
-// console.log(Boolean(NaN));
-// console.log(Boolean(false));
-
-// let a = 10;
-
-// console.log(Boolean(!!5));
-// console.log(10 + '');
-// console.log(a.toString());
-
-// console.log(+"10c");
-// console.log(+"10");
-
-// let str = +prompt("Cколько тебе лет?");
-// let result = str + 10;
-
-// console.log(result);
-
-
-// let str = prompt("Cколько тебе лет?");
-
-// console.log(parseInt(str));
-
-// console.log(parseInt('11111', 2));
-
-// if (true) console.log(' Истина');
-// if (false) console.log(' Ложь');
-
-// let n =14;
-
-// if (n == 11) {
-//   console.log(' Команда 1');
-//   console.log(' Команда 2');
-//   console.log(' Команда 3');
-// } else if (n == 12) {
-//   console.log(' Команда 3');
-//   console.log(' Команда 4');
-//   console.log(' Команда 5');
-// } else if (n == 13) {
-//   console.log(' Команда 6');
-//   console.log(' Команда 7');
-//   console.log(' Команда 8');
-// } else  {
-//   console.log(' Команда 9');
-//   console.log(' Команда 10');
-//   console.log(' Команда 11');
-// }
-// let n = 12;
-// switch (n) {
-//   case 3:
-//     console.log(3);
-//     break;
-//   case 4:
-//     console.log(4);
-//     break;
-//   case 10:
-//     console.log(10);
-//     break;
-//   case 11:
-//   case 12:
-//       console.log(11);
-//     break;
-//   default:
-//     console.log(' Не верно ни одно значение');
-// }
-// const n = +prompt(' Введите число : ');
-
-// switch (n) {
-  //   case 3:
-  //     console.log(3);
-  //     break;
-  //   case 4:
-  //     console.log(4);
-  //     break;
-  //   case 10:
-  //     console.log(10);
-  //     break;
-  //   case 11:
-  //   case 12:
-  //       console.log(11);
-  //     break;
-  //   default:
-  //     console.log(' Не верно ни одно значение');
-  // }
-
-//   const n = +prompt(' Введите число : ');
-
-
-// switch (true) {
-//   case 5 < n && n < 10:
-//     console.log('n > 10');
-//     break;
-//   default:
-//       console.log(' Не верно ни одно значение');
+// function one(){
+//   let a = 4
+//   console.log(a);
 // }
 
-// const n = +prompt(' Введите число : ');
+// one()
 
-// let result = n === 10 ? 1 : 2
-// console.log(result);
+// let a = 5
+
+// function one(){
+//   let a = 10
+//   function two(){
+//     let a = 15
+//     console.log(a);
+//   }
+//   two()
+// }
+
+// one()
+
+// let a = 5
+
+// function one(c, d){
+//   // LexicalEnviroment = (c: 3, d:undefined)
+//   let a = 10
+//   // LexicalEnviroment = (a: 10,c: 3, d:undefined)
+//   console.log(a, c, d);
+
+//   function two(){
+//     // LexicalEnviroment = ()
+//     // Scope{a: 10, c: 3, d:undefined}
+//     console.log(a, c);
+//   }
+//   two()
+// }
+
+// one(3)
+
+// let y = 5
+
+// function one(x){
+//   console.log(x + y);
+// }
+
+// function two () {
+//   let y = 15
+//   // хотч тут определна перменная Y все
+//   // равно будет считывать глобальную переменнюу н
+//   one(3)
+// }
+
+// two()
 
 
-// const n = +prompt(' Введите число : ');
+// ЗАМЫКАНИЕ  - это функция внитри функции
 
-// n === 10 ? console.log(" верно") : console.log(" не верно");;
+// function one(x) {
 
+//   function two(){
+//     console.log(x);
+//     console.dir(two);
+//   }
+
+//   two()
+// }
+// one(10)
+
+
+// function one(x) {
+
+//   function two(){
+//     const a = +prompt("Введите число")
+//     x--;
+
+//     console.log(a);
+//     console.log(x);
+
+//     if (a !== x){
+//       two()
+//     }
+//   }
+
+//   two()
+// }
+
+// one(10)
+
+
+// function counter(n){
+//   return function (x) {
+//     return x + n
+//     }
+// }
+
+// let sum = counter(1)
+// console.log(sum(15));
+// console.log(sum(20));
+
+
+function pathGenerator(url) {
+  return function(imgeName){
+    return url + imgeName
+  }
+}
+
+let urlToIcons = pathGenerator('http://mydomain.ru/assests/icons/')
+let urlToImages = pathGenerator('http://mydomain.ru/assests/images/')
+
+console.log(urlToIcons('clock.svg'));
+console.log(urlToIcons('man.jpg'));
